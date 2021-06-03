125b9abda0b99af1effd7fca93d71dba43b55b74
saptashree <79249701+saptashree@users.noreply.github.com>
2021-06-04 00:55:42 +0530

    updated
    up2

---
+++ MainNav.js
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import Home from "../Header/Home";
+
+const MainNav = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/"> Home </Link>
+          </li>
+          <li>
+            <Link to="/about"> About </Link>
+          </li>
+          <li>
+            <Link to="/products"> Products </Link>
+          </li>
+          <li>
+            <Link to="/contact"> Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+export default MainNav;
