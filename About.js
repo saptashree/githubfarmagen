bcbd1300811745f691e53f01605113935cda769a
saptashree bennur <saptashree.vb17@gmail.com>
2021-06-08 23:41:37 +0530

    updated about component

--- About.js
+++ About.js
@@ -1,6 +1,35 @@
+import { Container, Row, Col } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import classes from './About.module.css';
+
+
+
 const About=()=>{
     return(
-        <di> About</di>
+        <div>
+        <Container>
+            <div className={classes.container}>
+            <Row>
+                
+                <Col>
+                <h3>Who we really are</h3>
+                <h2>About <br></br>Farmagen</h2>
+                <p>Farmagen has emerged in order to provide quality seeds, easily accessible and affordable for farmers, Farmagen depends on Research and Development in the pursuit of innovation to serve its customers the best quality seeds they deserve
+
+                </p>
+                <button > Read More</button>
+
+                </Col>
+                <Col>
+                <div>
+                    <img src='/images/photo4.jpg' alt='doctor'></img>
+                </div>
+                </Col>
+                
+            </Row></div>
+        </Container>
+        </div>
     );
 };
 export default About;
